dbdc0c34293227e7f71dfd26adc3cf8aa6faf3c3
diotsani <90304533+diotsani@users.noreply.github.com>
2021-10-23 03:54:45 +0700

    Add files via upload

---
+++ Assets/Scripts/Player/PlayerDetector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public GameOverManager gameOverManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && !other.isTrigger)
+        {
+            float enemyDistance = Vector3.Distance(transform.position, other.transform.position);
+
+            gameOverManager.ShowWarning(enemyDistance);
+        }
+    }
+}
